4b3e87f68f566c8a47e1ccf022bbc003a5b39cfd
simbo1905 <simbo1905@60hertz.com>
2018-02-21 22:01:21 +0000

    env var in php-fpm

--- 7.1/contrib/services.sh
+++ 7.1/contrib/services.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# https://github.com/docker-library/php/issues/74
 # https://medium.com/@tomahock/passing-system-environment-variables-to-php-fpm-when-using-nginx-a70045370fad
-echo "clear_env = no" >> /etc/php-fpm.d/www.conf
+echo "\n\nclear_env = no" >> /etc/php-fpm.d/www.conf
 env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" >> /etc/php-fpm.d/www.conf
 
 php-fpm
