a3e4bece199bd16a3ef1f3a6d96d4a4ea754bd8b
simbo1905 <simbo1905@60hertz.com>
2018-02-21 20:24:09 +0000

    set env into /etc/php-fpm.d/env.conf

--- 7.1/contrib/services.sh
+++ 7.1/contrib/services.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# https://github.com/docker-library/php/issues/74
+env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /etc/php-fpm.d/env.conf
 php-fpm
 nginx -c /opt/app-root/etc/conf.d/nginx/nginx.conf
