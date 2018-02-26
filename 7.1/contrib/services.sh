8088096a4d88217ededd7be930a44a7380a82ae4
simbo1905 <simbo1905@60hertz.com>
2018-02-26 21:23:31 +0000

    #3 env vars values containing '=' are not passed through to php-fpm properly

--- 7.1/contrib/services.sh
+++ 7.1/contrib/services.sh
@@ -2,7 +2,7 @@
 
 # https://medium.com/@tomahock/passing-system-environment-variables-to-php-fpm-when-using-nginx-a70045370fad
 printf "\nclear_env = no\n" >> /etc/php-fpm.d/www.conf
-env | sed "s/\(.*\)=\(.*\)/env[\1] = '\2'/" >> /etc/php-fpm.d/www.conf
+env | sed "s/\([^=]*\)=\(.*\)/env[\1] = '\2'/" >> /etc/php-fpm.d/www.conf
 printf "\n" >> /etc/php-fpm.d/www.conf
 
 php-fpm
