96b88bf17a2ea83b0516d902e870a5e020b60fc1
simbo1905 <simbo1905@60hertz.com>
2018-02-26 22:20:11 +0000

    #4 php-fpm complains of

--- 7.1/contrib/services.sh
+++ 7.1/contrib/services.sh
@@ -2,7 +2,8 @@
 
 # https://medium.com/@tomahock/passing-system-environment-variables-to-php-fpm-when-using-nginx-a70045370fad
 printf "\nclear_env = no\n" >> /etc/php-fpm.d/www.conf
-env | sed "s/\([^=]*\)=\(.*\)/env[\1] = '\2'/" >> /etc/php-fpm.d/www.conf
+# remote empty values then rewrite as env[key] = 'value'
+env | awk -F '=' '$2 !~ /^$/{print}'  | sed "s/\([^=]*\)=\(.*\)/env[\1] = '\2'/" >> /etc/php-fpm.d/www.conf
 printf "\n" >> /etc/php-fpm.d/www.conf
 
 php-fpm
