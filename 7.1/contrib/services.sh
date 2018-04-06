c53cf2abba69b7a260c08f05513b7b782502a25d
simbo1905 <simbo1905@60hertz.com>
2018-04-06 21:44:41 +0100

    Logging goes to  which isn't set to Kibana on OpenShift Online #13

--- 7.1/contrib/services.sh
+++ 7.1/contrib/services.sh
@@ -6,8 +6,8 @@ printf "\nclear_env = no\n" >> /etc/php-fpm.d/www.conf
 env | awk -F '=' '$2 !~ /^$/{print}'  | sed "s/\([^=]*\)=\(.*\)/env[\1] = '\2'/" >> /etc/php-fpm.d/www.conf
 printf "\n" >> /etc/php-fpm.d/www.conf
 
-php-fpm
+php-fpm 2>&1
 nginx -c /opt/app-root/etc/conf.d/nginx/nginx.conf
 
 # this echo to stdout is needed otherwise the nginx stdout doesn't show up on the docker run output
-echo date
+echo date 2>&1
